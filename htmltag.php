4fa8d76fe4f44a08edaa7eb0c7e59371ff177300
Sreenivas harien Mone <sm2542@afsaccess1.njit.edu>
2017-10-20 19:46:36 -0400

    added htmltag code

---
+++ htmltag.php
@@ -0,0 +1,14 @@
+<?php
+
+class htmltag{
+
+
+   static public function horizontalRule(){
+   return'hr>';
+   
+   
+   static public function headingOne($text){
+   reurn'<h1>'.$text.'</h1>';
+ 
+	}
+}	
